53f02321db54d872a13b860d46fc5301e30175ea
Ivonne Terrero <iqcodes21@gmail.com>
2017-05-01 17:48:00 -0700

    changing to prioritize user Model.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 // import semantic from 'semantic-ui-react';
 import './profile.css';
-// import ProjectsList from '../Projects/projects_list';
+import ProjectsList from '../Projects/projects_list';
 import SkillsList from '../Skills/skills_list';
 import AddProjectForm from '../AddProject/add_project_form';
 import { connect } from 'react-redux';
-import firebase from 'firebase';
+// import firebase from 'firebase';
 import AddSkillsForm from '../Skills/skills_form';
-
+import { Link } from "react-router";
 function mapStateToProps({ userData }){
   return {
     userData
@@ -15,7 +15,6 @@ function mapStateToProps({ userData }){
 }
 
 const renderUserName = (userData) => {
-  console.log("In the renderUsername", userData);
   return (
           <div>{userData.name}</div>
   )
@@ -27,14 +26,21 @@ class Profile extends Component {
       <div>
         <div className="userPro">
           <h1>{renderUserName(this.props.userData)}</h1>
-
             <img className="ui circular image imgPro" src={this.props.userData.photo}></img>
         </div>
         <div>
           <SkillsList />
-          <AddProjectForm />
-          <AddSkillsForm />
-          {/* <ProjectsList /> */}
+          {/* <AddProjectForm /> */}
+          {/* <AddSkillsForm /> */}
+          <ProjectsList />
+          {/* <div> */}
+            <h1>Inside of the button div</h1>
+
+          {/* </div> */}
+        </div>
+        <div>
+          {this.props.children}
+          {/* <Link to='/skills_form'><button  className="btn btn-success">Add Skill</button></Link> */}
         </div>
       </div>
     );
